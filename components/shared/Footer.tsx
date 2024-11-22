01a6f7466cf4a5dc79bdf871c43758120657de7a
VijayLuxmiMaddheshiya <maddheshiyavijayluxmi2002@gmail.com>
2024-11-22 16:34:21 +0530

    change in footer text

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
           />
         </Link>
 
-        <p>2024 Evently. All Rights reserved.</p>
+        <p>2024 Evently. All Rights are reserved.</p>
       </div>
     </footer>
   )
