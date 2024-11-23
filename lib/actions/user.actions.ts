298fb2cdb123409d7638e7163b4b511b34d319e7
VijayLuxmiMaddheshiya <maddheshiyavijayluxmi2002@gmail.com>
2024-11-23 12:23:46 +0530

    change and improved typo

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -18,6 +18,7 @@ export async function createUser(user: CreateUserParams) {
     const newUser = await User.create(user)
     return JSON.parse(JSON.stringify(newUser))
   } catch (error) {
+    console.log(error)
     handleError(error)
   }
 }
@@ -31,6 +32,7 @@ export async function getUserById(userId: string) {
     if (!user) throw new Error('User not found')
     return JSON.parse(JSON.stringify(user))
   } catch (error) {
+    console.log(error)
     handleError(error)
   }
 }
@@ -44,6 +46,7 @@ export async function updateUser(clerkId: string, user: UpdateUserParams) {
     if (!updatedUser) throw new Error('User update failed')
     return JSON.parse(JSON.stringify(updatedUser))
   } catch (error) {
+    console.log(error)
     handleError(error)
   }
 }
@@ -77,6 +80,7 @@ export async function deleteUser(clerkId: string) {
 
     return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null
   } catch (error) {
+    console.log(error)
     handleError(error)
   }
 }
