dfa622219c00895ca09ea0c41cff542868c79ac5
VijayLuxmiMaddheshiya <maddheshiyavijayluxmi2002@gmail.com>
2024-11-23 13:23:25 +0530

    change middleware

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,22 @@
 
 import { authMiddleware } from "@clerk/nextjs";
+ 
 export default authMiddleware({
-    publicRoutes: [
-        '/',
-        '/events/:id',
-        '/api/webhooks/clerk',
-        '/api/uploadthing'
-      ],
-      ignoredRoutes: [
-        '/api/webhooks/clerk',
-        '/api/uploadthing'
-      ]
+  publicRoutes: [
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ],
+  ignoredRoutes: [
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ]
 });
  
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
- 
-
  
