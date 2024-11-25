0be693894eb62c555316d690c21265e55c6ebea8
Sakshi <sakshisihag157@gmail.com>
2024-11-25 17:07:09 +0530

    feat/made the create-event page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,13 @@
 import type { Config } from "tailwindcss";
-
+import { withUt } from "uploadthing/tw";
+ 
 export default {
     darkMode: ["class"],
     content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+	 "./src/**/*.{ts,tsx,mdx}"
   ],
   theme: {
   	extend: {
