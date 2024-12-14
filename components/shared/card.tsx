aa21a1d8703985eaf07340a6b21e1ebe8837ee48
Sakshi <sakshisihag157@gmail.com>
2024-12-14 14:12:28 +0530

    changed the layout of cards on homepage

--- components/shared/card.tsx
+++ components/shared/card.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { DeleteConfirmation } from './DeleteConfirmation'
+import { Bold } from 'lucide-react'
 
 type CardProps = {
   event: IEvent,
@@ -17,8 +18,9 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
   const userId = sessionClaims?.userId as string;
 
   const isEventCreator = userId === event.organizer._id.toString();
+
   return (
-    <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
+    <div className="group relative p-2 flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
       <Link 
         href={`/events/${event._id}`}
         style={{backgroundImage: `url(${event.imageUrl})`}}
@@ -36,26 +38,40 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
         </div>
       )}
 
-      <Link
+      <Link 
       href = {'/events/${event._id}'}
-      className = ""
+      className = "p-3"
       >
 
       {/* <div
         className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4"
       >  */}
-       {!hidePrice && <div className="flex gap-2">
-          <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
+       {!hidePrice && 
+        <div className="flex items-center space-x-4">
+          <p className="p-medium-16 p-medium-18 text-grey-500">
+            {formatDateTime(event.startDateTime).dateTime}
+          </p>
+          {/* Uncomment if needed */}
+          {/* <span className="p-semibold-14 w-min rounded-full bg-green-100 text-green-60">
             {event.isFree ? 'FREE' : `$${event.price}`}
-          </span>
+          </span> */}
           <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1">
             {event.category.name}
           </p>
-        </div>}
-
-        <p className="p-medium-16 p-medium-18 text-grey-500">
-          {formatDateTime(event.startDateTime).dateTime}
-        </p>
+        </div>
+          /*
+          <div className="flex gap-5">
+              <p className="p-medium-16 p-medium-18 text-grey-500">
+                {formatDateTime(event.startDateTime).dateTime}
+              </p>
+              <span className="p-semibold-14 w-min rounded-full bg-green-100 text-green-60">
+                {event.isFree ? 'FREE' : `$${event.price}`}
+              </span>
+              <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1">
+                {event.category.name}
+              </p>
+            </div> */
+        }
 
         <Link href={`/events/${event._id}`}>
           <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">{event.title}</p>
@@ -63,14 +79,8 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
 
         <div className="flex-between w-full">
           <p className="p-medium-14 md:p-medium-16 text-grey-600">
-            {event.organizer.firstName} {event.organizer.lastName}
+            Organiser: {event.organizer.firstName} {event.organizer.lastName}
           </p>
-          <p className="p-medium-14 md:p-medium-16 text-grey-600">
-            {event.organizer
-              ? `${event.organizer.firstName} ${event.organizer.lastName}`
-              : "Organizer information not available"}
-          </p>
-
 
           {hasOrderLink && (
             <Link href={`/orders?eventId=${event._id}`} className="flex gap-2">
